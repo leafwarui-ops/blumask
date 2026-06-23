18c488f564f5a8663e5286ed46c415c289324601
unknown <leafwarui@gmail.com>
2026-06-23 11:45:49 -0300

    começo do desenvolvimento

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<forms action = "">
+    <label for="">Nome de exibição:</label><!-- nome que aparece para os usuarios -->
+    <br>
+    <input type = "text" id = "nome_exibicao" nome = "nome_exibi"></input>
+    <br>
+    <label for="">Nome de usuario:</label><!-- nome de registro que pode ser unico -->
+    <br>
+    <input type = "text" id = "nome_exibicao" nome = "nome_exibi"></input>
+    <br>
+    <label for="">Senha:</label>
+    <br>
+    <input type = "password" id = "nome_exibicao" nome = "nome_exibi"></input>
+    <br>
+</forms>
