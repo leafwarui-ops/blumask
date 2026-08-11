593f0b9cecce2c934c5692404e9f6ce7265c99d0
Kirab <kirab@example.com>
2026-08-11 11:34:08 -0300

    style: Ajustes no alt da imagem de perfil

--- php/usr_edit.php
+++ php/usr_edit.php
@@ -80,7 +80,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         <div class="edit-panel-body">
           <div class="edit-avatar-wrapper">
-            <img src="https://i.pinimg.com/736x/87/46/76/874676100eb6085a8efb483c7ccfa89b.jpg" alt="Avatar do usuário" class="edit-avatar">
+            <img src="https://i.pinimg.com/736x/87/46/76/874676100eb6085a8efb483c7ccfa89b.jpg" alt="" class="edit-avatar">
             <button class="btn-edit-avatar" title="Editar Foto">
               <svg viewBox="0 0 24 24"><path fill="currentColor" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>
             </button>
